3ba74af410740f1628371b5634e32d1bad555a0d
wei0328 <weichchang0328@gmail.com>
2024-06-13 05:10:35 -0400

    added splash page

--- petcare_record/lib/main.dart
+++ petcare_record/lib/main.dart
@@ -9,6 +9,7 @@ import 'package:petcare_record/pages/map_page.dart';
 import 'package:petcare_record/dashboard/bottom_navigation_bar.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:petcare_record/pages/splash.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
       builder: (context, child) {
         return GetMaterialApp(
           debugShowCheckedModeBanner: false,
-          home: Login(),
+          home: Splash(),
         );
       },
     );
---
+++ petcare_record/lib/pages/splash.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:petcare_record/globalclass/color.dart';
+import 'package:petcare_record/pages/login.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    goup();
+  }
+
+  goup() async {
+    var navigator = Navigator.of(context);
+    await Future.delayed(const Duration(seconds: 5));
+    navigator.push(MaterialPageRoute(
+      builder: (context) {
+        return const Login();
+      },
+    ));
+  }
+
+  dynamic size;
+  double height = 0.00;
+  double width = 0.00;
+
+  @override
+  Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
+    height = size.height;
+    width = size.width;
+    return Scaffold(
+      backgroundColor: PetRecordColor.theme,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/logo/logo_white.png',
+              height: height / 3,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
